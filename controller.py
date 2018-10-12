dd613abcf94dd65ee5f3f2111b76408497462a3c
Frank Tronack <frankenstein91.info@googlemail.com>
2018-10-12 10:07:45 +0200

    solved an error with the new logging

--- controller.py
+++ controller.py
@@ -15,7 +15,7 @@ from logging.handlers import RotatingFileHandler
 logger = logging.getLogger("OTCC")
 logger.setLevel(logging.DEBUG)
 
-fh = logging.RotatingFileHandler('debug.log',maxBytes=20971520,backupCount=5)
+fh = RotatingFileHandler('debug.log',maxBytes=20971520,backupCount=5)
 fh.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.ERROR)
